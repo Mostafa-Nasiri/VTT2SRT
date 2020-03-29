258894daa7ba45785547e7ddaab99ec9e707f79d
mindsim <51875132+mindsim@users.noreply.github.com>
2020-03-30 01:44:55 +0430

    Update vtt2srt.py

--- vtt2srt.py
+++ vtt2srt.py
@@ -17,33 +17,6 @@ for entry in os.scandir('.'):
 
          if (data[20]==62): #time format check
             for j in range(len(times)-1) :
-             s=s+str(j+1)+chr(13)+chr(10)+'00:'+((data[times[j]+1:times[j]+23].decode(encoding='UTF-8')).replace('.',',')).replace('--> ','--> 00:')+chr(13)+chr(10)+(data[times[j]+24:times[j+1]].decode(encoding='UTF-8'))+chr(13)+chr(10)+chr(13)+chr(10)
-         else:
-             for j in range(len(times)-1) :
-              s=s+str(j+1)+chr(13)+chr(10)+((data[times[j]+1:times[j]+30].decode(encoding='UTF-8')).replace('.',','))+chr(13)+chr(10)+(data[times[j]+30:times[j+1]].decode(encoding='UTF-8'))+chr(13)+chr(10)+chr(13)+chr(10)
-
-         fp = open(os.path.splitext(entry.name)[0]+".srt", "wb") #Write srt file
-         fp.write(s.encode('utf-8'))
-         fp.close()
-#WebVTT to SRT Converter
-#Mostafa Nasiri
-
-import os
-
-for entry in os.scandir('.'):
-    if entry.is_file():
-        if os.path.splitext(entry.name)[1]==('.vtt'):   #List vtt File in directory
-         fp = open(entry.name, "rb")
-         data = fp.read()
-         fp.close()
-         times=[]
-         s='' #output string variable
-         for i in range(len(data)) : #Search for timelaps
-             if (data[i]+data[i-1]==20):times.append(i)
-         times.append(len(data))
-
-         if (data[20]==62): #time format check
-            for j in range(len(times)-1) :
              s=s+str(j+1)+"\r\n"+'00:'+((data[times[j]+1:times[j]+23].decode(encoding='UTF-8')).replace('.',',')).replace('--> ','--> 00:')+"\r\n"+(data[times[j]+24:times[j+1]].decode(encoding='UTF-8'))+"\r\n"+"\r\n"
          else:
              for j in range(len(times)-1) :
